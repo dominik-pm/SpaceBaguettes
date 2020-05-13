361c9ee0f83bda2299e29f928eb9e003957968ed
Dominik Pömmer <dominik25664@gmail.com>
2020-05-13 16:06:47 +0200

    ups

--- Previous/Project/Scenes/Game/Player/HitBox.gd
+++
@@ -1,8 +0,0 @@
-extends KinematicBody2D
-
-class_name PlayerHitbox
-
-var player
-
-func _ready():
-	player = get_parent()
--- Previous/Project/Scenes/MainMenu/Rocket.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _ready():
-	$Flame.play("flying")
-	$AnimationPlayer.play("flying")
